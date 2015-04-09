8a1e4414f6863b294eac0f38eb2d6ab09f2c58b0
Borgella <jmborgella@hotmail.com>
2015-04-08 23:08:05 -0400

    Erreur de message de creation rep a partir de 3 rep imbrique

--- newdir.c
+++ newdir.c
@@ -11,8 +11,6 @@
 #include <errno.h>
 #include "fonction.h"
 
-void
-createFolder(char* tampon,char **arguments,int taille);
 
 int main(int agrc, char*argv[]){
     errno = 0;
@@ -33,67 +31,3 @@ int main(int agrc, char*argv[]){
 		createFolder(tampon,arguments,taille);				
 	return 0;
 }
-
-void
-createFolder(char* tampon,char **arguments,int taille){
-    DIR *repertoire;
-    struct dirent *dp;
-    int true;
-    static int i = 0;
-    static int reussite = 0;
-    if ((repertoire = opendir(tampon)) != NULL) {
-        chdir(tampon);
-        while ((dp = readdir(repertoire))!= NULL) {
-            true = strcmp(dp->d_name,"..") != 0 && strcmp(dp->d_name,".") != 0;
-            if(true) {
-				if(strcmp(dp->d_name,arguments[i])==0){
-					i++;
-					if(i < taille){
-						createFolder(arguments[i-1],arguments,taille);
-					}
-					chdir(arguments[taille-1]);
-					if(!reussite){
-						reussite = mkdir(arguments[taille],0777);
-						if(reussite == -1)
-							fprintf(stderr,"Impossible de créer le répertoire.\n"); 
-						else
-							printf("Répertoire créé.\n");
-						break;
-					}			
-				}   
-			}
-        }
-     }
-     closedir(repertoire);
-}
-
-
-
-
-
-
-/*
-void
-createFolder(char* tampon,char *folder,char *name){
-    DIR *repertoire;
-    struct dirent *dp;
-    int true,reussite;
-    if ((repertoire = opendir(tampon)) != NULL) {
-        chdir(tampon);
-        while ((dp = readdir(repertoire))!= NULL) {
-            true = strcmp(dp->d_name,"..") != 0 && strcmp(dp->d_name,".") != 0;
-            if(true) {
-				if(strcmp(dp->d_name,folder)==0){
-					chdir(folder);
-					reussite = mkdir(name,0777);
-					if(reussite == -1)
-						fprintf(stderr,"Impossible de créer le repertoire.\n"); 
-				     else
-						printf("Repertoire crée.\n");
-					 break;
-					}   
-				}
-            }
-        }
-}
-*/
