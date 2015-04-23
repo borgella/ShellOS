2a073387f0888f0cb0b38343709382d036fff224
Borgella <jmborgella@hotmail.com>
2015-04-23 15:51:55 -0400

    respect de la norme pour appel fork parent - enfant

--- tsh.c
+++ tsh.c
@@ -20,49 +20,27 @@ int main(int argc, char **argv)
     while(1)
     {
         fgets(tampon, LIMITE, stdin);
-        tailleCommande = tailleTableauDArguments(tampon);
-        if (tailleCommande >= 1) {
-              arguments = creerArguments(tampon);
-              if((strcmp(arguments[0],"cd") == 0) || (strcmp(arguments[0],"cdir") == 0) || ((strcmp(arguments[0],"exit") == 0))){
-					executerCommandeTsh(tailleCommande,arguments);
-					afficherTurboShell(tampon);											
-			  }else
-			  {
-				  afficherTurboShell(tampon);
-				  enfant = fork();
-				  if(enfant < 0){
-						fprintf(stderr,"Impossible de creer un nouveau processus");
-						exit(1);
-				   }
-				  if(enfant){
-					   wait(&etat);
-				  }else if(enfant == 0){   
-						    if(commandeValide(arguments,tailleCommande)){
-								execve(arguments[0],arguments,NULL);
-								fprintf(stderr,"Erreur d'execve \n");
-							}else fprintf(stderr,"Programme introuvable.\n");
-            
-				}
-			  }
-                
-        }else
-            {
+        afficherTurboShell(tampon);
+        enfant = fork();
+        if(enfant < 0){
+			fprintf(stderr,"Impossible de creer un nouveau processus");
+			exit(1);
+		}else if(enfant  == 0){   
+			tailleCommande = tailleTableauDArguments(tampon);
+            if (tailleCommande >= 1) {
+                arguments = creerArguments(tampon);
+                if(commandeValide(arguments,tailleCommande)){
+					execve(arguments[0],arguments,NULL);
+					fprintf(stderr,"Erreur d'execve");
+                }else if((strcmp(argv[0],"cd") == 0) || (strcmp(argv[0],"cdir")) || (strcmp(argv[0],"exit"))){
+					executerCommandeTsh(argc,argv);											
+				}else fprintf(stderr,"Programme introuvable.\n");
+            }else{
                printf("tsh>");
             }
+        }else if(enfant){
+            waitpid(enfant,&etat,1);
+        }
     }
     return 0;
 }
-
-
-
-		
-            
-            
-            
-            
-             
-            
-            
-            
-            
-            
