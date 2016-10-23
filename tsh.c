1d5b298c8f4d11727e00cde239a2b1a02096e54d
Borgella JM <jmborgella@hotmail.com>
2016-10-23 19:43:01 -0400

    Add files via upload

--- tsh.c
+++ tsh.c
@@ -11,36 +11,74 @@
 #define	 LIMITE 500
 
 int main(int argc, char **argv)
-{	
+{
     char tampon[LIMITE];
     char **arguments;
-    int tailleCommande, etat;
+    char *indirection;
+    int tailleCommande, etat,retour_methodes;
     pid_t enfant;
     printf("tsh>");
+    fflush(stdout);
     while(1)
     {
         fgets(tampon, LIMITE, stdin);
-        afficherTurboShell(tampon);
         enfant = fork();
-        if(enfant < 0){
-			fprintf(stderr,"Impossible de creer un nouveau processus");
+        if(enfant < 0)
+        {
+			fprintf(stderr,"impossible de creer de nouveau processus \n");
 			exit(1);
-		}else if(enfant  == 0){   
+		}
+        else if(enfant == 0)
+        {	
 			tailleCommande = tailleTableauDArguments(tampon);
             if (tailleCommande >= 1) {
                 arguments = creerArguments(tampon);
                 if(commandeValide(arguments,tailleCommande)){
-					execve(arguments[0],arguments,NULL);
-					fprintf(stderr,"Erreur d'execve");
-                }else if((strcmp(argv[0],"cd") == 0) || (strcmp(argv[0],"cdir")) || (strcmp(argv[0],"exit"))){
-					executerCommandeTsh(argc,argv);											
-				}else fprintf(stderr,"Programme introuvable.\n");
-            }else{
+					retour_methodes = aUneIndirection(arguments);
+					if(retour_methodes)
+					{
+						indirection = trouverIndirection(arguments);
+						setIndirectionDuProgramme(indirection,arguments,tailleCommande);
+					}
+					if(estUneCommandeTsh(arguments[0]))
+					{
+						getcwd(tampon,256);
+						if(strcmp(arguments[0],"cd") == 0){
+							retour_methodes = commandeCd(tampon,arguments[1]);
+							if(!retour_methodes)
+								fprintf(stderr,"Repertoire inexistant.\n");
+						}else if(strcmp(arguments[0],"cdir") == 0){
+							if(tampon != NULL)
+								printf("Répertoire courant : %s \n", tampon);
+								fflush(stdout);
+						}else{
+							kill(enfant,SIGKILL);
+						}
+					 }else {
+						 execve(arguments[0],arguments,NULL);
+						 fprintf(stderr,"Erreur d'execve,les programmes ne se trouvent pas dans ce repertoires.\n");
+					 }
+                
+                }else {
+					if(strcmp(arguments[0],"fin") == 0)
+						fprintf(stderr,"Argument Invalide.\n");
+					else{
+						fprintf(stderr,"Programme Invalide.\n");
+					}	
+				}
+            }
+            else
+            {	
                printf("tsh>");
             }
-        }else if(enfant){
-            waitpid(enfant,&etat,1);
+            
         }
+        else
+        {   
+			waitpid(enfant,&etat,0);
+	    }
+        afficherTurboShell(tampon);
+        fflush(stdout);
     }
     return 0;
 }
